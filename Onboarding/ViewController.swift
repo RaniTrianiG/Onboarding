04c26355f513751a76b78bf8010563d2d6f175ed
RaniTrianiG <ranitrianig@gmail.com>
2020-04-13 21:23:03 +0700

    Initial Commit

---
+++ Onboarding/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Onboarding
+//
+//  Created by Rani Triani Gustia on 13/04/20.
+//  Copyright © 2020 Rani Triani Gustia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
